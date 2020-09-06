74e706ab2346c3a74df576a931b167a538a4b932
AntonioAlva1310 <smatechbeat@gmail.com>
2020-09-05 20:18:13 -0600

    Adding Axios to use Api

---
+++ src/components/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Keep track of your tasks! Create and Delete them easily.</p>
+        </React.Fragment>
+    )
+}
